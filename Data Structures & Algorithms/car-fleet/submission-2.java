3174cc4dbe60d11bbaa679433a99c9193da0ff12
Amit Pathak <amitkpathak09031997@gmail.com>
2026-06-30 22:55:24 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        ArrayDeque<Double> stack = new ArrayDeque<>();
+
+        int[][] cars = new int[position.length][2];
+        for(int i=0; i<position.length; i++) {
+            cars[i][0]=position[i];
+            cars[i][1]=speed[i];
+        }
+        Arrays.sort(cars, (a,b)->Integer.compare(a[0],b[0]));
+        System.out.println(Arrays.deepToString(cars));
+        for(int i=0; i<position.length; i++) {
+            double time = (double)(target-cars[i][0])/cars[i][1];
+            stack.push(time);
+        }
+        System.out.println(stack);
+        int fleets = 1;
+        double t = stack.pop();
+        while(!stack.isEmpty()) {
+            double time=stack.pop();
+            if(t>=time) {
+                continue;
+            } else {
+                fleets+=1;
+                t=time;
+            }
+        }
+        return fleets;
+    }
+}
